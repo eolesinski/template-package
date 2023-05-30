0ff752a2c0e26d5d76c2362ece71f9ca5ace5d55
Ethan Olesinski <47125809+eolesinski@users.noreply.github.com>
2023-05-30 18:54:16 -0400

    Update hello.py

--- template-project/hello.py
+++ template-project/hello.py
@@ -1,9 +1,7 @@
 # create a new function with type annotation that returns the string hello world
 def hello() -> str:
-
     return "Hello, world!"
 
 # create a function with type annotation that prints the the hello function
 def print_hello() -> None:
-
     print(hello())
