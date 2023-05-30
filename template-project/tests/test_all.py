dee7356c274c8ae5a0c388820b7ee3352d72dbc2
Ethan Olesinski <47125809+eolesinski@users.noreply.github.com>
2023-05-30 18:54:31 -0400

    Update test_all.py

--- template-project/tests/test_all.py
+++ template-project/tests/test_all.py
@@ -3,14 +3,10 @@ from unittest.mock import patch
 
 # create a test function that tests the hello function
 def test_hello():
-
     assert hello() == "Hello, world!"
 
 # mock out the hello funtion to tests that the print hellp function works
 @patch('builtins.print')
-
 def test_print_hello(mock_print):
-
     print_hello()
-
     assert mock_print.call_args.args == ("Hello, world!",)
