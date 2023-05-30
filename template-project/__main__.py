3f5b2899140af244c66340dd9f61de003d4903b3
Ethan Olesinski <47125809+eolesinski@users.noreply.github.com>
2023-05-30 19:11:54 -0400

    Delete template-project directory

--- template-project/__main__.py
+++
@@ -1,4 +0,0 @@
-from .hello import print_hello
-
-if __name__ == "__main__":  # pragma: no cover
-    print_hello()  # pragma: no cover
--- template-project/hello.py
+++
@@ -1,7 +0,0 @@
-# create a new function with type annotation that returns the string hello world
-def hello() -> str:
-    return "Hello, world!"
-
-# create a function with type annotation that prints the the hello function
-def print_hello() -> None:
-    print(hello())
