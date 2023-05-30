a5fe64eae746ecfddba062c5c2638afe9b148a9f
[eolesinski] <eo2454@columbia.edu>
2023-05-30 17:48:24 -0400

    initial commit

---
+++ template-project/tests/test_all.py
@@ -0,0 +1,16 @@
+from example_project_python import hello, print_hello
+from unittest.mock import patch
+
+# create a test function that tests the hello function
+def test_hello():
+
+    assert hello() == "Hello, world!"
+
+# mock out the hello funtion to tests that the print hellp function works
+@patch('builtins.print')
+
+def test_print_hello(mock_print):
+
+    print_hello()
+
+    assert mock_print.call_args.args == ("Hello, world!",)
